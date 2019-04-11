87df0083fa247200274ab1541828a43d87c262b2
andres pacheco <andrespach4@gmail.com>
2019-04-10 22:58:54 -0500

    nuevos cambios

---
+++ primeras_funciones.py
@@ -0,0 +1,26 @@
+numero1=5
+numero2=7
+if numero1>numero2:
+	print("numero 1 es el mayor")
+else:
+	print("numero2 es el mayor ")
+
+def suma(numero1,numero2):
+	print(numero1+numero2)
+suma(12,13)
+
+# edad
+print("bienvenido")
+edad_usuario = int(input("ingresa tu edad porfavor"))
+if edad_usuario < 18:
+	print("edad incorracta no puedes pasar")
+else:
+	print("bienvenido")
+
+
+
+
+
+
+
+
