79942b4e588bf619686d9d69c32fcf241cb97217
andres pacheco <andrespach4@gmail.com>
2019-04-11 19:33:26 -0500

    nuevo cambio

---
+++ beca_alumno.py
@@ -0,0 +1,17 @@
+distancia_escuela = 60
+edad = 19
+numero_hermanos = 3
+ingreso_familia = 3000
+#nombre = input("ingresa tu nombre por favor ")
+edad = int(input("ingresa tu edad porfavor "))
+distancia_escuela = int(input("que distancia vives de la escuela "))
+numero_hermanos = int(input("cuantos hermanos tienes "))
+if edad <=19 and distancia_escuela >=60 and numero_hermanos >=3 and ingreso_familia <= 3000:
+    print("tienes derecho a  beca")
+
+    print("tienes derecho a beca")
+        
+else:
+    print("no tienes derecho a beca ")
+    
+
